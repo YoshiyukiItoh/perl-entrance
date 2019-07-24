b0180b50d7b56b8bda4e8f8cb185398b73d6fda7
yoshi <yoshi@mbp17.local>
2019-07-24 20:24:57 +0900

    prac file add

---
+++ part4/practice/vote.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+# key -> name, value -> favorite_foods(array)
+my 
+
+my %hash1 = (
+	name => 'taro',
+	favorite_foods =>
+);
+
+my hash1_ref = \%hash1;
+
+my hash2_ref = {}
