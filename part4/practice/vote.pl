0707e7c60f17ab6718df713818c077c4ea23de9d
Yoshiyuki Ito <yoshiyuki.ito.biz@gmail.com>
2019-07-25 05:52:10 +0900

    1-2 end

--- part4/practice/vote.pl
+++ part4/practice/vote.pl
@@ -1,15 +1,28 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use Data::Dumper;
 
-# key -> name, value -> favorite_foods(array)
-my 
+my @array = ('ichigo','melon','lemon');
+my $array_ref = \@array;
 
 my %hash1 = (
 	name => 'taro',
-	favorite_foods =>
+	favorite_foods => $array_ref
 );
 
-my hash1_ref = \%hash1;
+my $hash1_ref = \%hash1;
 
-my hash2_ref = {}
+#print Dumper \$hash1_ref;
+
+my $hash2_ref = {
+	name => 'jiro',
+	favorite_foods => ['ichigo2','melon2','lemon2']
+};
+
+#print Dumper \$hash2_ref;
+
+my @hash_arrays = ($hash1_ref, $hash2_ref);
+
+print Dumper \@hash_arrays;
+
