93f4a68d38b98068eb99d257f87c4f9a4e7549c5
yoshi <yoshi@mbp17.local>
2019-08-04 16:34:28 +0900

    add 2 score.pl

---
+++ part4/practice/score.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $alice = {
+    name        => 'Alice',
+    country     => 'England',
+    perl        => 60,
+    python      => 80,
+    ruby        => 80,
+    php         => 50,
+    binary      => 30,
+};
+
