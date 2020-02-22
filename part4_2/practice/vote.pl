05f133eb54ac132d9e3c11e72c3d0e64b1018ac4
Yoshiyuki Ito <swallows.vct1a19@gmail.com>
2020-02-22 19:47:30 +0900

    1-3.

--- part4_2/practice/vote.pl
+++ part4_2/practice/vote.pl
@@ -5,27 +5,30 @@ use Data::Dumper;
 
 my $hash_ref1 = {
   name => 'ito',
-  favorite_foods => ["bread", "pizza"]
+  favorite_foods => ["bread", "pizza", "beef", "pork"]
 };
 
 my $hash_ref2 = {
   name => 'tanaka',
-  favorite_foods => ["beef", "cheese"]
+  favorite_foods => ["beef", "cheese", "pork", "bread"]
 };
 
 my $hash_ref3 = {
   name => 'sato',
-  favorite_foods => ["pork", "rice"]
+  favorite_foods => ["pork", "rice", "pizza"]
 };
 
 my @array = ($hash_ref1, $hash_ref2, $hash_ref3);
 
-#print Dumper \@array;
-
+my %food_ranking = ();
 for my $hash (@array) {
-  print "-----" . "\n";
-  for my $key (sort keys %{$hash}) {
-    #print $key . "\n";
-    print Dumper $hash->{$key};
+  for my $food (@{$hash->{'favorite_foods'}}){
+    $food_ranking{$food}++;
   }
+}
+
+#print Dumper \%food_ranking;
+
+for my $key (reverse sort {$food_ranking{$a} <=> $food_ranking{$b}} keys %food_ranking) {
+  print "$key : $food_ranking{$key}" . "\n";
 }
